a9a9aa09643e0409dfd4416250fd66fc0a983ce7
Dakuan <dom.barker808@gmail.com>
2016-11-24 10:59:42 +0000

    call extraArg if its a function

--- src/index.js
+++ src/index.js
@@ -1,7 +1,13 @@
 function createThunkMiddleware(extraArgument) {
   return ({ dispatch, getState }) => next => action => {
+    let argument;
+    if (typeof extraArgument === 'function') {
+      argument = extraArgument(dispatch, getState);
+    } else {
+      argument = extraArgument;
+    }
     if (typeof action === 'function') {
-      return action(dispatch, getState, extraArgument);
+      return action(dispatch, getState, argument);
     }
 
     return next(action);
