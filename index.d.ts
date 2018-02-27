4eb7404565cdf0696f3c96955d492c87e03d7e2a
Wang Zhongliang <zwang@rebelworks.io>
2018-02-28 00:09:22 +0800

    Remove unnecessary default values for some generic types

--- index.d.ts
+++ index.d.ts
@@ -1,21 +1,21 @@
 import { Middleware, Action, AnyAction } from "redux";
 
 
-export type ThunkAction<R, S = {}, E = {}, A extends Action<any> = AnyAction> = (
+export type ThunkAction<R, S, E, A extends Action> = (
   dispatch: ThunkDispatch<S, E, A>,
   getState: () => S,
   extraArgument: E
 ) => R;
 
-interface ThunkDispatch<S = {}, E = {}, A extends Action = AnyAction> {
+interface ThunkDispatch<S, E, A extends Action> {
   <T extends A>(action: T): T;
 }
 
-interface ThunkDispatch<S = {}, E = {}, A extends Action = AnyAction> {
+interface ThunkDispatch<S, E, A extends Action> {
   <R>(asyncAction: ThunkAction<R, S, E, A>): R;
 }
 
-type ThunkMiddleware<E, S = {}> = Middleware<ThunkDispatch<S, E>, S, ThunkDispatch<S, E>>;
+type ThunkMiddleware<E, S = {}, A extends Action = AnyAction> = Middleware<ThunkDispatch<S, E, A>, S, ThunkDispatch<S, E, A>>;
 
 declare const thunk: ThunkMiddleware<undefined> & {
   withExtraArgument<E>(extraArgument: E): ThunkMiddleware<E>
