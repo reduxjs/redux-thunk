d2da4888a45fda53fa4d834736edd9c195e97592
Aymen Chebbi <aymen.chebi@gmail.com>
2018-07-31 17:02:49 +0100

    Reduced codebase

--- src/index.js
+++ src/index.js
@@ -1,14 +1,9 @@
 function createThunkMiddleware(extraArgument) {
-  return ({ dispatch, getState }) => next => action => {
-    if (typeof action === 'function') {
-      return action(dispatch, getState, extraArgument);
-    }
-
-    return next(action);
-  };
+  return ({ dispatch, getState }) => next =>
+    action => typeof action === 'function' ?
+      action(dispatch, getState, extraArgument)
+      : next(action);
 }
-
 const thunk = createThunkMiddleware();
 thunk.withExtraArgument = createThunkMiddleware;
-
 export default thunk;
