00328862b12ab30b475925e3acb83a74489fa244
Oliver Zeigermann <oliver.zeigermann@gmail.com>
2021-10-14 14:39:35 +0200

    Update index.js
    obviously better 🗡️

--- src/index.js
+++ src/index.js
@@ -1,11 +1,5 @@
 function createThunkMiddleware(extraArgument) {
-  return ({ dispatch, getState }) => (next) => (action) => {
-    if (typeof action === 'function') {
-      return action(dispatch, getState, extraArgument);
-    }
-
-    return next(action);
-  };
+  return ({ dispatch, getState }) => (next) => (action) => typeof action === 'function' ? action(dispatch, getState, extraArgument) : next(action);
 }
 
 const thunk = createThunkMiddleware();
