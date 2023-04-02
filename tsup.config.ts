4806e1528dda77bad9647a81d07ac6fbd1c3fd3d
Mark Erikson <mark@isquaredsoftware.com>
2023-04-02 13:34:37 -0400

    Use tsup for build config and update package formatting for ESM

---
+++ tsup.config.ts
@@ -0,0 +1,24 @@
+import { defineConfig, Options } from 'tsup'
+
+export default defineConfig(options => {
+  const commonOptions: Partial<Options> = {
+    entry: ['src/index.ts'],
+    ...options
+  }
+
+  return [
+    {
+      ...commonOptions,
+      format: ['esm'],
+      outExtension: () => ({ js: '.mjs' }),
+      dts: true,
+      clean: true
+    },
+    {
+      ...commonOptions,
+      format: 'cjs',
+      outDir: './dist/cjs/',
+      outExtension: () => ({ js: '.cjs' })
+    }
+  ]
+})
