b37f8106a2ae9c8038a1df7b3a53711f1d6fe7d5
Arya Emami <aryaemami59@yahoo.com>
2024-09-12 08:05:10 -0500

    Fix type issues in `scripts/writeGitVersion.mjs`

--- scripts/writeGitVersion.mjs
+++ scripts/writeGitVersion.mjs
@@ -1,13 +1,9 @@
-import fs from 'node:fs'
-import path from 'node:path'
-import { fileURLToPath } from 'node:url'
-
-const __filename = fileURLToPath(import.meta.url)
-const __dirname = path.dirname(__filename)
+import * as fs from 'node:fs'
+import * as path from 'node:path'
 
 const gitRev = process.argv[2]
 
-const packagePath = path.join(__dirname, '../package.json')
+const packagePath = path.join(import.meta.dirname, '../package.json')
 const pkg = JSON.parse(fs.readFileSync(packagePath, 'utf8'))
 
 pkg.version = `${pkg.version}-${gitRev}`
